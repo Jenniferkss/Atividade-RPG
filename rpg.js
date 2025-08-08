b7929c236f1975ccc93de4634829fa1d2ec59e38
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-08-07 22:51:29 -0300

    feat: adiciona novas partes ao capitulo 1

--- rpg.js
+++ rpg.js
@@ -5,7 +5,7 @@ let vida = 100;
 let ouro = 80;
 let xp = 60;
 
-console.log(`LORE DA HEROINA: Ketniss 🏹🔥`);
+console.log(`LORE DA HEROÍNA: Ketniss 🏹🔥`);
 console.log(`${nome}, faz parte da classe dos ${classe} da floresta encantada de Panem, é conhecida por sua mira infalível e coragem inabalável.`);
 
 const NOME_ARMA = "Arco de fogo";
@@ -22,7 +22,7 @@ ouroRestante = ouro -= 30;
 pontosDeVidaRecuperados = vida += 40;
 armaEncantada =  danoBase *= 2;
  
-console.log(`Após muitas missões cumpridas, acumula ${pontosBonusTreino} de XP e carrega ${ouroRestante} moedas de ouro, apos precisar comprar uma poção de recuperação, ela esta pronta para negociações em tavernas distantes.`);
+console.log(`Após muitas missões cumpridas, acumula ${pontosBonusTreino} de XP e carrega ${ouroRestante} moedas de ouro, após precisar comprar uma poção de recuperação, ela esta pronta para negociações em tavernas distantes.`);
 console.log(`Com sua vida plena em ${pontosDeVidaRecuperados} pontos, ela segue preparada para o próximo confronto.`);
 
 ataqueFinal = nivel + danoBase;
@@ -36,7 +36,13 @@ ataqueForte = ataqueFinal > 60;
 nivelAvancado = nivel >= 10;
 podeEnferntarGuardiao = vidaSulficiente && (ataqueForte || nivelAvancado)
 
-console.log(`Será que ${nome} podera enfrentar o guardião?? ${podeEnferntarGuardiao}`);
+console.log(`Será que ${nome} poderá enfrentar o guardião??`);
+if (podeEnferntarGuardiao = true) {
+    console.log(`${nome} está pronta e poderá enfrentar o guardião`);
+} else {
+    console.log(`${nome} ainda não esta preparada para este grande desafio.`);
+    
+}
 console.log(`Com seu arco em punho e o coração firme, Ketniss avança para enfrentar o guardião, sabendo que só a coragem e a habilidade podem garantir a vitória`);
 
 let vidaMaxima = 100;
@@ -49,12 +55,12 @@ let missaoAtual = "Derrotar o guardião";
 
 // Prólogo
 console.log('PRÓLOGO');
-console.log(`Após tres meses de treinamento intensivo ${nome} vem como uma grande ameça para o guardião. Será que ela o vencerá?? Antes disso ela precisa enfrentar uma série de desafios até o chegar no grande guardião`);
+console.log(`Após três meses de treinamento intensivo ${nome} vem como uma grande ameça para o guardião. Será que ela o vencerá?? Antes disso ela precisa enfrentar uma série de desafios até o chegar no grande guardião`);
 
 // Capitulo 1 
 console.log("CAPITULO 1: Olympo");
 
-console.log(`Antes de começar os desafios ${nome} deve escolher por qual caminho seguirá, sendo estes o misterioso caminho do Olympo e o arrepilante caminho do Jaguar. Qual sera que nossa arqueira escolherá??`);
+console.log(`Antes de começar os desafios ${nome} deve escolher por qual caminho seguirá, sendo estes o misterioso caminho do Olympo e o arrepiante caminho do Jaguar. Qual será que nossa arqueira escolherá??`);
 
 let CaminhoOlympo = true; 
 if (CaminhoOlympo) {
@@ -64,14 +70,49 @@ if (CaminhoOlympo) {
 if(!CaminhoOlympo) {
     xp = xp/2;
 }
-console.log(`${nome} fez uma escolha arriscada mas dessa vez acertou!! Escolhendo o Olympo e ganhou +20 xp que podem ser necessarios mais pra frente. Entretanto o primeiro desafio do olympo nao depende apenas dela mas sim de sua quantidade de ouro ja que, precisara comprar uma capa de invisibilidade para passar pelos dementadores sem ser vista`);
+console.log(`${nome} fez uma escolha arriscada,mas dessa vez acertou!! ${nome} escolheu o misterioso caminho do Olympo, ganhando +20 XP — uma decisão arriscada que pode valer muito a pena.Entretanto, o primeiro desafio do Olympo não depende apenas dela mas, também da sua quantidade de ouro ja que, precisará comprar uma capa de invisibilidade para passar pelos dementadores sem ser vista`);
 
 if (ouro >= 200 ) {
-    console.log(`Que sorte, nossa heroina consegue passar pelos dementadores sem problemas`);
+    console.log(`Que sorte, nossa heroína consegue passar pelos dementadores sem problemas`);
     
 } else {
-    console.log(`Dessa vez nao deu para nossa guerreira passar ilesa, os dementadores a viram e agora ela tera que derotalos`);
+    console.log(`Dessa vez não deu para nossa guerreira passar ilesa, os dementadores a viram e agora ela tera que derotá-los`);
     
 }
+
 let flechaEncatada = ataqueFinal * 3; 
-//console.log(`${nome}`);
+let vidaDementador = 100;
+let dementadoresRestantes = 3; 
+let danoRestante = flechaEncatada;
+
+console.log(`${nome} deve lutar contra 3 dementadores; se ela ganhar a batalha, poderá continuar sua missão se perder... algo ruim pode lhe acontecer.Ela deve derotar os 3 dementadores se um deles resistir ela deve escolher entre seu ataque surpresa ou enfrentá-los novamente correndo riscos de perder bons pontos de vida. E lá vai ela...`);
+
+if (danoRestante >= 100) {
+    dementadoresRestantes --;
+    danoRestante -= 100;
+    console.log(`Nossa heroína ja derrotou um dos dementadores, agora restam ${dementadoresRestantes}`); 
+}  else {
+    console.log(`O primeiro dementador resistiu ao ataque de ${nome}!! ainda restam ${dementadoresRestantes}`);
+}
+
+if (danoRestante >= 100) {
+    dementadoresRestantes --;
+    danoRestante -= 100;
+    console.log(`E ela vence mais um!!! Agora restam ${dementadoresRestantes}`);
+} else {
+    console.log(`Mais um dementador resistente! O que nossa heroína fará agora?? Ela tentara destriuir o proximo dementador ou usará um ataque supresa para destrui-los de uma só vez?`);
+} 
+let ataqueSurpresa = danoRestante * 80; 
+
+if (dementadoresRestantes >= 2) {
+    vidaDementador -= ataqueSurpresa;
+    dementadoresRestantes = 0;
+    console.log(`Todos os dementadores foram derrotados,ou seja restam ${dementadoresRestantes}`);
+}   else if (dementadoresRestantes === 1) {
+     vida -= 50; 
+     console.log(`Agora ${nome} deve escolher entre seu ataque surpresa ou enfrentar novamente os dementadores.O que será que ela fará??`);
+     
+    console.log(`Ela tenta enfrentá-los novamente, mas os dementadores a ferem, e sua vida cai para ${vida}`);
+}   else {
+   console.log(`Todos os dementadores foram derrotados e nossa heroína pode avançar sem sua missão!`);
+}
